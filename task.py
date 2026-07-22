0ab41aed6f92e152b180479edf15c07acce25feb
Evgeny Levchenko <ljekyll666@yandex.ru>
2026-07-22 23:51:11 +0300

    Add task.py

---
+++ task.py
@@ -0,0 +1,123 @@
+import datetime
+
+class OnlineSalesRegisterCollector:
+
+    def __init__(self):
+        self.__name_items = []
+        self.__number_items = 0
+        self.__item_price = {'чипсы': 50, 'кола': 100, 'печенье': 45, 'молоко': 55, 'кефир': 70}
+        self.__tax_rate = {'чипсы': 20, 'кола': 20, 'печенье': 20, 'молоко': 10, 'кефир': 10}
+
+    @property
+    def name_items(self):
+        return self.__name_items
+
+    @property
+    def number_items(self):
+        return self.__number_items
+
+    @property
+    def item_price(self):
+        return self.__item_price
+
+    @property
+    def tax_rate(self):
+        return self.__tax_rate
+
+    def add_item_to_cheque(self, name_item):
+        try:
+            if len(name_item) == 0 or len(name_item) > 40:
+                raise ValueError('Нельзя добавить товар, если в его названии нет символов или их больше 40')
+        
+            if name_item not in self.__item_price:
+                raise NameError('Позиция отсутствует в товарном справочнике')
+        
+            self.__name_items.append(name_item)
+            self.__number_items += 1
+    
+        except ValueError as e:
+            print(e)
+        except NameError as e:
+            print(e)
+
+    def delete_item_from_check(self, name_item):
+        try:
+            if name_item not in self.__name_items:
+                raise NameError('Позиция отсутствует в чеке')
+            self.__name_items.remove(name_item)    
+            self.__number_items -= 1
+        except NameError as e:
+            print(e)
+
+    def check_amount(self):
+        total = []
+    
+        for item in self.__name_items:
+            total.append(self.__item_price[item])
+    
+        if len(self.__name_items) > 10:
+            return sum(total) * 0.9
+    
+        return sum(total)
+
+    def twenty_percent_tax_calculation(self):
+        twenty_percent_tax = []
+        total = []
+
+        for item in self.__name_items:
+            if self.__tax_rate.get(item) == 20:
+                twenty_percent_tax.append(item)
+                total.append(self.__item_price[item])
+
+        if len(self.__name_items) > 10:
+            total = [price * 0.9 for price in total]
+
+        return sum(total) * 0.2
+
+
+    def ten_percent_tax_calculation(self):
+        ten_percent_tax = []
+        total = []
+
+        for item in self.__name_items:
+            if self.__tax_rate.get(item) == 10:
+                ten_percent_tax.append(item)
+                total.append(self.__item_price[item])
+
+        if len(self.__name_items) > 10:
+            total = [price * 0.9 for price in total]
+
+        return sum(total) * 0.1
+
+    def total_tax(self):
+        return self.twenty_percent_tax_calculation() + self.ten_percent_tax_calculation()
+
+    @staticmethod
+    def get_telephone_number(telephone_number):
+        try:
+            tel_str = str(int(telephone_number))
+        except (ValueError, TypeError):
+            raise ValueError('Необходимо ввести цифры')
+        if len(tel_str) != 10:
+            raise ValueError('Необходимо ввести 10 цифр после "+7"')
+        return f"+7{tel_str}"
+
+register = OnlineSalesRegisterCollector()
+
+register.add_item_to_cheque('чипсы')
+register.add_item_to_cheque('кола')
+register.add_item_to_cheque('молоко')
+register.add_item_to_cheque('кефир')
+
+print("Товары в чеке:", register.name_items)
+print("Количество товаров:", register.number_items)
+print("Сумма чека:", register.check_amount())
+print("НДС 20%:", register.twenty_percent_tax_calculation())
+print("НДС 10%:", register.ten_percent_tax_calculation())
+print("Общий НДС:", register.total_tax())
+
+print("Телефон:", OnlineSalesRegisterCollector.get_telephone_number(9991234567))
+
+register.add_item_to_cheque('')
+register.add_item_to_cheque('несуществующий')
+register.delete_item_from_check('хлеб')
